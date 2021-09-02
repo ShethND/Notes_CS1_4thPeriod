9ff314825557f57709c85f97e60c1e514577d409
ShethND <nsheth@newtech.coppellisd.com>
2021-09-02 15:36:20 -0500

    Revert "Update Notes01_Printing_and_Escape_Sequences.java"
    This reverts commit 37e03f12962c17aff171a8e8227933b7ad07161c.

---
+++ src/notes/notes01.java
@@ -0,0 +1,33 @@
+package notes;
+
+public class notes01 {
+public static void main(String[] args) {
+		
+		// print() Simply prints the String
+		System.out.print("Sup yo! ");
+		
+		// println() prints the String and then a return
+		System.out.println("😊");
+		System.out.println("Mr Hunter loves CS1");
+		
+		/*
+		 * 
+		 * Escape Sequences:
+		 * 		\"  actually print a "  (don't end the string)
+		 * 		\n  go to the next line (print a return)
+		 * 		\\  actually prints a \ (don't start an escape sequence)
+		 */
+		System.out.println("Adam said, \"Im tired!\"");
+		System.out.println("When I was\ngoing to Saint Eyve's");
+		System.out.println("\\");
+		
+		problem1();
+		
+	}
+	
+	public static void problem1() {
+		
+		System.out.println("Thand GOD tomorrow is Flex!");
+		
+	}
+}
